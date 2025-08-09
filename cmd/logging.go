c05e78273e176522d0af493abe4c2b6d2b8df721
jmuchiri <mmjames451@gmail.com>
2025-08-09 17:11:56 +0300

    feat: add new logging function

---
+++ cmd/logging.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"log"
+	"os"
+)
+
+// logToFile handles logging to "app.log".
+// If the file doesn't exist, it creates it.
+// If it can't open/create the file, it logs to stderr instead.
+func logToFile(message string) {
+	// Open or create the file in append mode
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Println("Could not open or create log file, logging to stderr:", err)
+		log.Println(message)
+		return
+	}
+	defer file.Close()
+
+	// Create a logger that writes to the file
+	logger := log.New(file, "INFO: ", log.LstdFlags)
+	logger.Println(message)
+}
